31f8c91b7f5ba7e64bf10cfee42e827e7f221708
Patrick Kim <pkim050@gmail.com>
2019-04-03 17:27:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' {"My name is __"}
+
+  get '/hometown' {"My hometown is __"}
+
+  get '/favorite-song' {"My favorite song is __"}
 end
