6dcc4b663f307ad23c70693d442a463fe41f9127
Patrick Kim <pkim050@gmail.com>
2019-04-03 17:28:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,7 +5,11 @@ class App < Sinatra::Base
     "My name is __"
   end
 
-  get '/hometown' {"My hometown is __"}
+  get '/hometown' do
+    "My hometown is __"
+  end
 
-  get '/favorite-song' {"My favorite song is __"}
+  get '/favorite-song' do
+    "My favorite song is __"
+  end
 end
